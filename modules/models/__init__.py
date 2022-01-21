770a9a298756a4937b88d03e40a683d949dd9d83
Kaylode <kayp.kieran@gmail.com>
2022-01-21 13:55:33 +0700

    add covidnet

--- modules/models/__init__.py
+++ modules/models/__init__.py
@@ -1,11 +1,14 @@
 from .base_model import BaseModel
 from .classifier import Classifier
 from .models import BaseTimmModel
+from .covidnet import CovidNet
 
 def get_model(config, num_classes):
 
-    net = BaseTimmModel(
-        name=config.model_name, 
-        num_classes=num_classes)
-
+    if config.model_name == 'covidnet':
+        net = CovidNet('large', num_classes=num_classes)
+    else:
+        net = BaseTimmModel(
+            name=config.model_name, 
+            num_classes=num_classes)
     return net
