b0629413f975e668bd1c14387a619cf2f6e54357
kaylode <kayp.kieran@gmail.com>
2023-01-02 19:54:09 +0000

    :art: clean up

--- source/detection/models/__init__.py
+++ source/detection/models/__init__.py
@@ -1,7 +1,5 @@
 from source.classification.models import MODEL_REGISTRY
 from .detr_convnext import DETRConvnext
-from .detrsegm_convnext import DETRSegmConvnext
 from .wrapper import ModelWithLossandPostprocess
 
 MODEL_REGISTRY.register(DETRConvnext)
-MODEL_REGISTRY.register(DETRSegmConvnext)
