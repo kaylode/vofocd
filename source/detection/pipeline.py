2b003a30195b98786640ba33d4d2a013f8a5bb4e
kaylode <kayp.kieran@gmail.com>
2023-01-02 20:52:20 +0000

    :art: fix a bit

--- source/detection/pipeline.py
+++ source/detection/pipeline.py
@@ -1,8 +1,11 @@
+import torch
 from source.detection.models import MODEL_REGISTRY
 from source.detection.losses import LOSS_REGISTRY
-from theseus.cv.detection.pipeline import BasePipeline
+from theseus.cv.detection.pipeline import Pipeline
+from theseus.base.utilities.loading import load_state_dict
+from theseus.base.utilities.download import download_from_url
 
-class Pipeline(BasePipeline):
+class DetPipeline(Pipeline):
     """docstring for Pipeline."""
 
     def __init__(
@@ -16,3 +19,20 @@ class Pipeline(BasePipeline):
         super().init_registry()
         self.model_registry = MODEL_REGISTRY
         self.loss_registry = LOSS_REGISTRY
+
+    def init_loading(self):
+        self.resume = self.opt['global']['resume']
+        self.pretrained = self.opt['global']['pretrained']
+        self.last_epoch = -1
+        if self.pretrained:
+            if self.pretrained.startswith('https'):
+                self.pretrained = download_from_url(self.pretrained)
+            state_dict = torch.load(self.pretrained, map_location='cpu')
+            self.model.model.model = load_state_dict(self.model.model.model, state_dict, strict=False, key='model')
+
+        if self.resume:
+            state_dict = torch.load(self.resume, map_location='cpu')
+            self.model.model = load_state_dict(self.model.model, state_dict, 'model')
+            self.optimizer = load_state_dict(self.optimizer, state_dict, 'optimizer')
+            iters = load_state_dict(None, state_dict, 'iters')
+            self.last_epoch = iters//len(self.train_dataloader) - 1
