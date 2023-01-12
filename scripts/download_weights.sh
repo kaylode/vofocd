60c9f7de541c19b2536ca4f2021ff8f9e53700ee
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-12 11:10:28 +0000

    :fire: Clean up scripts files

--- scripts/download_weights.sh
+++
@@ -1,4 +0,0 @@
-mkdir weights
-cd weights
-wget https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth
-cd ..
--- scripts/train_segm.sh
+++
@@ -1,6 +0,0 @@
-EXP_NAME=detr
-PYTHONPATH=. python3 tools/train_segm.py \
-              -c configs/segmentation/detrsegm.yaml \
-              -o global.save_dir=runs \
-              global.exp_name=$EXP_NAME \
-              global.exist_ok=True
