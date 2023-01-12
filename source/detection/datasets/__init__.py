f1666ca8ad63b9634786ec31ab2186a33fa7fa77
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-12 11:47:06 +0000

    :bento: Add files from theseus for easy modification

--- source/detection/datasets/__init__.py
+++ source/detection/datasets/__init__.py
@@ -1,5 +1,7 @@
 from theseus.base.datasets import DATALOADER_REGISTRY, DATASET_REGISTRY
 
 from .vocal import VocalDetectionDataset
+from .coco import COCODataset
 
 DATASET_REGISTRY.register(VocalDetectionDataset)
+DATASET_REGISTRY.register(COCODataset)
