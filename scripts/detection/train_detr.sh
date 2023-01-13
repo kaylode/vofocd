56190c60cbd9b815931c3e247dbcfc58fc6b8ea8
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-13 10:32:57 +0000

    :sparkles: Using new configuration upgrade from theseus

--- scripts/detection/train_detr.sh
+++ scripts/detection/train_detr.sh
@@ -1,6 +1,6 @@
 EXP_NAME=detr
 PYTHONPATH=. python3 tools/train_det.py \
-              -c configs/detection/detr.yaml \
+              -c configs/detection/detr_convnext.yaml \
               -o global.save_dir=runs \
               global.exp_name=$EXP_NAME \
               global.exist_ok=False
