d4a8fa1505593ca9ec8b65e4d74c1442d2e1df5d
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-18 12:27:57 +0000

    :label: Update load pretrained

--- scripts/detection/train_detr.sh
+++ scripts/detection/train_detr.sh
@@ -1,6 +1,7 @@
-EXP_NAME=detr_convnext_kvasir
+MODEL_NAME=$1
+EXP_NAME=detr_${MODEL_NAME}_kvasir
 PYTHONPATH=. python3 tools/train_det.py \
-              -c configs/detection/detr_convnext.yaml \
+              -c configs/detection/detr_${MODEL_NAME}.yaml \
               -o global.save_dir=runs \
               global.exp_name=$EXP_NAME \
               global.exist_ok=False
--- source/detection/pipeline.py
+++ source/detection/pipeline.py
@@ -5,6 +5,8 @@ from source.detection.losses import LOSS_REGISTRY
 from theseus.cv.detection.pipeline import DetectionPipeline
 from theseus.base.utilities.loggers import LoggerObserver
 
+from theseus.base.utilities.loading import load_state_dict
+
 class DetPipeline(DetectionPipeline):
     """docstring for Pipeline."""
 
@@ -24,3 +26,25 @@ class DetPipeline(DetectionPipeline):
         self.logger.text(
             "Overidding registry in pipeline...", LoggerObserver.INFO
         )
+
+    def init_loading(self):
+        self.last_epoch = -1
+        if getattr(self, "pretrained", None):
+            state_dict = torch.load(self.pretrained, map_location="cpu")
+            self.model.model = load_state_dict(self.model.model, state_dict, "model", strict=False)
+
+        pretrained_backbone = self.opt['global'].get('pretrained_backbone', None)
+        if pretrained_backbone:
+            state_dict = torch.load(pretrained_backbone, map_location="cpu")
+            self.model.model.model.backbone[0].body = load_state_dict(
+                self.model.model.model.backbone[0].body, 
+                state_dict, 
+                key="model", strict=False
+            )
+
+        if getattr(self, "resume", None):
+            state_dict = torch.load(self.resume, map_location="cpu")
+            self.model.model = load_state_dict(self.model.model, state_dict, "model")
+            self.optimizer = load_state_dict(self.optimizer, state_dict, "optimizer")
+            iters = load_state_dict(None, state_dict, "iters")
+            self.last_epoch = iters // len(self.train_dataloader) - 1
