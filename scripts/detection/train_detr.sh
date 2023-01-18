2b5443fd34fe3365f23bff433eaaf5044141e074
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-18 12:07:53 +0000

    :zap: Update tools

--- scripts/detection/train_detr.sh
+++ scripts/detection/train_detr.sh
@@ -1,6 +1,6 @@
-EXP_NAME=detr
+EXP_NAME=detr_convnext_kvasir
 PYTHONPATH=. python3 tools/train_det.py \
-              -c configs/detection/detr_r50.yaml \
+              -c configs/detection/detr_convnext.yaml \
               -o global.save_dir=runs \
               global.exp_name=$EXP_NAME \
               global.exist_ok=False
