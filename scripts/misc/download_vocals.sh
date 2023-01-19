65412b89fa3f23150a862f1f179280d214bb0a38
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-19 16:29:26 +0000

    :globe_with_meridians: update download scripts

--- scripts/misc/download_vocals.sh
+++ scripts/misc/download_vocals.sh
@@ -2,10 +2,10 @@ mkdir data
 mkdir data/aim
 cd data/aim
 
-gdown 1oq1UDJfVRXOR8S9frbsEeg7Trmi_i5zL -O annotation_4cls.json
-gdown 1lqt9csAJGveWTRHMs6LIqEMesIDrLFpR -O annotation_5cls.json
-gdown 1xQAONKOKHwl1UV7JngBw4eJvj2fKxywx -O Train4classes.zip
-gdown 1stv13UMd0N-4vPPvR5tFSosA2TofvKmQ -O Train5classes.zip
+/bin/bash download_from_drive.sh 1oq1UDJfVRXOR8S9frbsEeg7Trmi_i5zL annotation_4cls.json
+/bin/bash download_from_drive.sh 1lqt9csAJGveWTRHMs6LIqEMesIDrLFpR annotation_5cls.json
+/bin/bash download_from_drive.sh 1xQAONKOKHwl1UV7JngBw4eJvj2fKxywx Train4classes.zip
+/bin/bash download_from_drive.sh 1stv13UMd0N-4vPPvR5tFSosA2TofvKmQ Train5classes.zip
 
 unzip Train4classes.zip
 unzip Train5classes.zip
