7c6cd7bb90372770b5ce1762d0d09e220cfe23e3
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-19 23:26:52 +0000

    :monocle_face: Update download scripts

--- scripts/misc/download_from_drive.sh
+++ scripts/misc/download_from_drive.sh
@@ -1,6 +1,6 @@
 FILE_ID=$1
 OUTPATH=$2
 
-GGDRIVE_PATH = "'https://docs.google.com/uc?export=download&id=${FILE_ID}'"
+GGDRIVE_PATH="'https://docs.google.com/uc?export=download&id=${FILE_ID}'"
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate ${GGDRIVE_PATH} -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=${FILE_ID}" -O $OUTPATH && rm -rf /tmp/cookies.txt
