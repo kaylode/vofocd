aacde6f945917abc4ec723ba76839b74227261a3
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-20 16:47:43 +0000

    :card_file_box: Critical fix folder

--- scripts/misc/download_vocals.sh
+++ scripts/misc/download_vocals.sh
@@ -1,5 +1,6 @@
 mkdir data
 mkdir data/aim
+mkdir data/aim/images
 
 /bin/bash scripts/misc/download_from_drive.sh 1oq1UDJfVRXOR8S9frbsEeg7Trmi_i5zL data/aim/annotation_4cls.json
 /bin/bash scripts/misc/download_from_drive.sh 1lqt9csAJGveWTRHMs6LIqEMesIDrLFpR data/aim/annotation_5cls.json
@@ -8,6 +9,6 @@ mkdir data/aim
 
 /bin/bash scripts/misc/download_from_drive.sh 1ASZEXGhqsIVJUpiJ5DuYkdBqAcMjDb0P data/aim/annotation_4cls_folds.zip
 
-unzip data/aim/Train4classes.zip -d data/aim/
-unzip data/aim/Train5classes.zip -d data/aim/
+unzip data/aim/Train4classes.zip -d data/aim/images
+unzip data/aim/Train5classes.zip -d data/aim/images
 unzip data/aim/annotation_4cls_folds.zip -d data/aim/
