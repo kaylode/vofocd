5706458a77ca81909ebfcc60decf576457e11846
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-20 12:33:57 +0000

    :wrench: Clean up scripts

--- scripts/detection/train_detr.sh
+++ scripts/detection/train_detr.sh
@@ -1,7 +1,7 @@
 MODEL_NAME=$1
 EXP_NAME=detr_${MODEL_NAME}_kvasir
 PYTHONPATH=. python3 tools/train_det.py \
-              -c configs/detection/detr_${MODEL_NAME}.yaml \
+              -c configs/detection/models/detr_${MODEL_NAME}.yaml \
               -o global.save_dir=runs \
               global.exp_name=$EXP_NAME \
               global.exist_ok=False
