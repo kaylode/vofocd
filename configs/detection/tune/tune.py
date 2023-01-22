f7da1fcdf0239b04a37af0aea9319b8daccaf9dd
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-01-22 00:05:20 +0000

    :wrench: Update training configuration and hyperparameters tuning

--- configs/detection/tune/tune.py
+++ configs/detection/tune/tune.py
@@ -1,4 +1,5 @@
 import os
+import optuna
 from theseus.opt import Config
 from theseus.base.utilities.optuna_tuner import OptunaWrapper
 from source.detection.pipeline import DetPipeline
@@ -7,17 +8,20 @@ load_dotenv()
 
 if __name__ == "__main__":
     config = Config("configs/detection/tune/detr_tune.yaml")
+    seed = config['global'].get('seed', 1702)
     database = os.getenv("POSTGRESQL_OPTUNA")
-    tuner = OptunaWrapper(storage=database)
 
+    tuner = OptunaWrapper(storage=database)
     tuner.tune(
         config=config,
-        study_name="detr_convnext_tune",
+        study_name="detr_convnext_pico_tune",
         pipeline_class=DetPipeline,
         best_key="f1_score",
-        n_trials=5,
+        n_trials=30,
         direction="maximize",
-        save_dir="runs/optuna/",
+        save_dir="runs/optuna",
+        sampler=optuna.samplers.RandomSampler(seed=seed),
+        pruner=optuna.pruners.MedianPruner()
     )
 
     
