b7846f86f2f6d6e3de1e475392157b6b215164d5
louisdo2108 <louisdo2108@gmail.com>
2023-02-24 07:36:24 +0000

    minor fix for detection infer

--- scripts/misc/download_wandb_weights.sh
+++ scripts/misc/download_wandb_weights.sh
@@ -1,4 +1,5 @@
-PYTHONPATH=. python3 tools/download_wandb_weights.py \
+%cd /home/htluc/vocal-folds/tools/
+PYTHONPATH=. python3 download_wandb_weights.py \
 --filename "checkpoints/best.pth" \
---run_path "hcmus-dcu/aim/11rjaz8p" \
---rename "resnet_vocal_8_best.pth"
+--run_path "hcmus-dcu/aim/fr9g3812" \
+--rename "tf_effnet_b0_fold_0_best.pth"
