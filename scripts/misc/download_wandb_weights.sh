6bac8a5ec88350b84ec6986313982f38e87e424b
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-02-24 10:52:05 +0000

    :fire: update download script

--- scripts/misc/download_wandb_weights.sh
+++ scripts/misc/download_wandb_weights.sh
@@ -1,4 +1,5 @@
 PYTHONPATH=. python3 tools/download_wandb_weights.py \
 --filename "checkpoints/best.pth" \
---run_path "hcmus-dcu/aim/11rjaz8p" \
---rename "resnet_vocal_8_best.pth"
+--run_path "hcmus-dcu/aim/fr9g3812" \
+--save_dir /mnt/4TBSSD/pmkhoi/isbi/weights/picoconvnext_vocal_0
+
