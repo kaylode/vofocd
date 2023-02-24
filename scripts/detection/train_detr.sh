400680b174092447be7541d61ce40e6f8a2ad4bb
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-02-24 16:25:54 +0000

    :zap: updatre configs

--- scripts/detection/train_detr.sh
+++ scripts/detection/train_detr.sh
@@ -6,5 +6,7 @@ PYTHONPATH=. python3 tools/detection/train_det.py \
               -o global.exp_name=$EXP_NAME \
               global.exist_ok=False \
               model.args.backbone_name=${MODEL_NAME} \
+              global.pretrained_backbone=weights/${MODEL_NAME}_${FOLD_ID}/checkpoints/best.pth \
+              global.pretrained=weights/detr-r50-e632da11.pth \
               data.dataset.train.args.label_path=data/aim/annotations/annotation_${FOLD_ID}_train.json \
               data.dataset.val.args.label_path=data/aim/annotations/annotation_${FOLD_ID}_val.json
