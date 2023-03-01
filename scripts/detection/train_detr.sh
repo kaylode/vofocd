9cf6608bd9f219e4b79ab492bddfd8b7f777415a
dtpthao <dtpthao@phoenix1>
2023-03-01 23:17:15 +0000

    :book: update scripts

--- scripts/detection/train_detr.sh
+++ scripts/detection/train_detr.sh
@@ -1,11 +1,11 @@
 MODEL_NAME=$1
 FOLD_ID=$2
 EXP_NAME=detr_${MODEL_NAME}_fold${FOLD_ID}_det
-PYTHONPATH=. python3 tools/detection/train_det.py \
+CUDA_VISIBLE_DEVICES=0 PYTHONPATH=. python3 tools/detection/train_det.py \
               -c configs/detection/models/detr_custom.yaml \
               -o global.exp_name=$EXP_NAME \
               global.exist_ok=False \
               model.args.backbone_name=${MODEL_NAME} \
-              global.pretrained=/mnt/4TBSSD/pmkhoi/isbi/runs/detr_convnext_pico_fold0_det_0/checkpoints/last.pth \
+              global.pretrained=weights/detr-r50-e632da11.pth \
               data.dataset.train.args.label_path=data/aim/annotations/annotation_${FOLD_ID}_train.json \
               data.dataset.val.args.label_path=data/aim/annotations/annotation_${FOLD_ID}_val.json
