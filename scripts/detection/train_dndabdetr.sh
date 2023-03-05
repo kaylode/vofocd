7fa1da264a962e9cdf7cbcd9be886bc592ff5e98
kaylode <pmkhoi@selab.hcmus.edu.vn>
2023-03-05 20:47:44 +0000

    :sparkles: add config dyhead, deformable, ...

--- scripts/detection/train_dndabdetr.sh
+++ scripts/detection/train_dndabdetr.sh
@@ -1,10 +1,10 @@
 MODEL_NAME=$1
 FOLD_ID=$2
-EXP_NAME=dndabdetr_${MODEL_NAME}_fold${FOLD_ID}_det
+EXP_NAME=dndabdetr_${MODEL_NAME}_fold${FOLD_ID}_test
 PYTHONPATH=. python3 tools/detection/train_det.py \
-              -c configs/detection/models/dn_dab_detr_custom.yaml \
+              -c /mnt/4TBSSD/pmkhoi/isbi/configs/detection/models/dn_dab_detr_custom.yaml \
               -o global.exp_name=$EXP_NAME \
-              global.exist_ok=False \
+              global.exist_ok=True \
               global.pretrained=/mnt/4TBSSD/pmkhoi/isbi/weights/DN-DAB-DETR-R50-DC5.pth \
               model.args.backbone_name=${MODEL_NAME} \
               data.dataset.train.args.label_path=data/aim/annotations/annotation_${FOLD_ID}_train.json \
