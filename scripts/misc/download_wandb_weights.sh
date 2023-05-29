43bc2aa575f2dc433ae678cec5a1515e20c1fb2b
kaylode <kayp.kieran@gmail.com>
2023-05-29 13:18:26 +0100

    remove unecessary files

--- scripts/misc/download_wandb_weights.sh
+++
@@ -1,5 +0,0 @@
-%cd /home/htluc/vocal-folds/tools/
-PYTHONPATH=. python3 download_wandb_weights.py \
---filename "checkpoints/best.pth" \
---run_path "hcmus-dcu/aim/fr9g3812" \
---rename "tf_effnet_b0_fold_0_best.pth"
